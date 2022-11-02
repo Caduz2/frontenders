11dfa070c4980c31399365b8c223373d27d84e26
patrickwarley <patrickwarley@gmail.com>
2022-11-02 19:53:24 -0300

    Cores da pagina inicial escolhidas

--- js/index.js
+++ js/index.js
@@ -3,3 +3,31 @@ function showPage() {
     document.getElementById('loader').classList.add('hide');
   }, 1000);
 }
+
+//Adicionando as animações na list de necessidades
+function startListAnimation(entries, observer) {
+  entries.forEach(entry => {
+    var listItems = document.querySelectorAll('.list-item');
+    if (entry.isIntersecting) {
+      return listItems.forEach((item, index) => setTimeout(() => item.classList.add('show'), index * 500))
+    }
+
+    return listItems.forEach((item, index) => setTimeout(() => item.classList.remove('show'), index * 500))
+  })
+}
+
+//observer options
+var options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+}
+
+function init() {
+  let observer = new IntersectionObserver(startListAnimation, options);
+  let detalhes = document.getElementById('detalhes');
+
+  observer.observe(detalhes);
+}
+
+init();
